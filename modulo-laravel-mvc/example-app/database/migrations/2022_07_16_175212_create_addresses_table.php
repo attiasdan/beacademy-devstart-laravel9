1af7cdd73bb53da08df658d454c56dfd81729097
Daniel Attias <danielattias@live.com>
2022-07-16 14:06:59 -0400

    update

--- modulo-laravel-mvc/example-app/database/migrations/2022_07_16_175212_create_addresses_table.php
+++ modulo-laravel-mvc/example-app/database/migrations/2022_07_16_175212_create_addresses_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('country');
             $table->timestamps();
         });
     }
