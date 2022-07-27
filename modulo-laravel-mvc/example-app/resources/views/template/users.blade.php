640672c287ca3a900279328c89322aa1968998a6
Daniel Attias <danielattias@live.com>
2022-07-26 20:35:41 -0400

    update

--- modulo-laravel-mvc/example-app/resources/views/template/users.blade.php
+++ modulo-laravel-mvc/example-app/resources/views/template/users.blade.php
@@ -6,14 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/app.css">
     <title>@yield('title')</title>
-    <style>
-        body {
-            background-color: #333333;
-            color: whitesmoke;
-            padding: 40px;
-        }
-    </style>
 </head>
 <body>
     <div class="container">
