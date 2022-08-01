5d97d54b43f6975155e3eda85491648060462f0c
Daniel Attias <danielattias@live.com>
2022-08-01 00:26:17 -0400

    update

--- modulo-laravel-mvc/example-app/app/Http/Controllers/UserController.php
+++ modulo-laravel-mvc/example-app/app/Http/Controllers/UserController.php
@@ -12,6 +12,10 @@ use function GuzzleHttp\Promise\queue;
 
 class UserController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
     public function index()
     {
         $users = User::all();
@@ -29,13 +33,20 @@ class UserController extends Controller
     public function create() {
         return view('users.create');
     }
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         // dd($request->all());
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
+
+        // $user = new User;
+        // $user->name = $request->name;
+        // $user->email = $request->email;
+        // $user->password = bcrypt($request->password);
+        // $user->save();
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        
+        $this->model->create($data);
 
         return redirect()->route('users.index');
     }
