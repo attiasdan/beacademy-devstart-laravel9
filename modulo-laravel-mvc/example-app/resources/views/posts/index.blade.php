d603c0e100cbada3979856d6b4fbd980604daf61
Daniel Attias <danielattias@live.com>
2022-08-22 23:36:21 -0400

    update

--- modulo-laravel-mvc/example-app/resources/views/posts/index.blade.php
+++ modulo-laravel-mvc/example-app/resources/views/posts/index.blade.php
@@ -6,22 +6,24 @@
     {{-- <a href="{{ route('users.create') }}" class="btn btn-success">Novo Usuário</a> --}}
 
     <table class="table text-white">
-        <thead>
+        <thead class="text-center">
             <tr>
                 <th scope="col">ID</th>
+                <th scope="co">AUTOR</th>
                 <th scope="col">TITULO</th>
                 <th scope="col">POSTAGEM</th>
                 <th scope="col">DATA CADASTRO</th>
                 <th scope="col">AÇÕES</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody class="text-center">
             @foreach($posts as $post)
                 <tr>
                     <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->user->name }}</td>
                     <td>{{ $post->title }}</td>
                     <td>{{ $post->post }}</td>
-                    <td>{{ date('d/m/Y', strtotime($post->created_at)) }}</td>
+                    <td>{{ date('d/m/Y - H:i', strtotime($post->created_at)) }}</td>
                     <td>
                         <a href="{{ route('users.show', $post->id) }}" class="btn btn-info">VISUALIZAR</a>
                     </td>
